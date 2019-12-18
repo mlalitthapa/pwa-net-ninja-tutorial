7a553fb050b5d98de09e1bc0374a80af21244a20
Lalit Thapa <lalit.thapa@ekbana.info>
2019-12-18 14:56:10 +0545

    Cache static assets

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
+const STATIC_ASSETS_CACHE = 'static-assets-cache';
+const ASSETS = [
+    '/',
+    'index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/materialize.min.css',
+    '/css/styles.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 // install service worker
-self.addEventListener('install', (event) => {
-    console.log('Service worker installed');
+self.addEventListener('install', event => {
+    // console.log('Service worker installed');
+    event.waitUntil(
+        caches.open(STATIC_ASSETS_CACHE)
+            .then(cache => {
+                console.log('caching assets');
+                cache.addAll(ASSETS);
+            })
+    );
 });
 
 // Listen to activate event
 self.addEventListener('activate', (event) => {
-    console.log('Service worker has been activated');
+    // console.log('Service worker has been activated');
 });
 
 // Fetch event
