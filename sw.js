fca67a9e916ed34602fe668b13f1d65a3deb7269
Lalit Thapa <lalit.thapa@ekbana.info>
2019-12-18 14:19:01 +0545

    Listen to activate event

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', (event) => {
     console.log('Service worker installed');
 });
+
+// Listen to activate event
+self.addEventListener('activate', (event) => {
+    console.log('Service worker has been activated');
+});
